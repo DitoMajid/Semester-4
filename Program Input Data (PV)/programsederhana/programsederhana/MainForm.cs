0f4d3b937966d88c274d1c9d8c7e45af1173298c
DitoMajid <92878061+DitoMajid@users.noreply.github.com>
2022-02-27 19:52:53 +0700

    Add files via upload

---
+++ Program Input Data (PV)/programsederhana/programsederhana/MainForm.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 27/02/2022
+ * Time: 19:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace programsederhana
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Add(textBox1.Text);
+		}
+	}
+}
